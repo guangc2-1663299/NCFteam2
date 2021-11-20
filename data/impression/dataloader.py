f47bcdb8d6d2f966c5361db1d664fe6709c44b71
jacky <guangyuan327@gmail.com>
2021-11-20 14:12:46 -0800

    fix labels and features

--- data/impression/dataloader.py
+++ data/impression/dataloader.py
@@ -9,17 +9,17 @@ from tqdm import tqdm
 def load_all(params):
     """ We load all the three file here to save time in each epoch. """
     train_data = pd.read_csv(os.path.join('data', params.data_dir, params.train_rating), dtype={0: np.int32, 1: np.int32})
-    train_data = train_data[['user_index','mlog_index']]
+    train_data = train_data[['user_index','mlog_index','isClick']]
 
     user_num = train_data['user_index'].nunique()
     item_num = train_data['mlog_index'].nunique()
 
-    train_data = train_data.values.tolist()
+    train_data2 = train_data.values.tolist()
 
     print('load ratings as a dok matrix')
     train_mat = sp.dok_matrix((user_num, item_num), dtype=np.float32)
-    for x in tqdm(train_data):
-        train_mat[x[0], x[1]] = 1.0
+    for x in tqdm(train_data2):
+        train_mat[x[0], x[1]] = x[2]
 
     test_data = pd.read_csv(os.path.join('data', params.data_dir, params.test_negative), dtype={0: np.int32, 1: np.int32})
     test_data = test_data[['user_index','mlog_index']].values.tolist()
@@ -32,7 +32,11 @@ class TrainSet(data.Dataset):
         """ Note that the labels are only useful when training, we thus 
             add them in the ng_sample() function.
         """
-        self.features_ps = features
+        #self.features_ps = features
+        features_ps = []
+        for x in features:
+            features_ps.append(x[:2])
+        self.features_ps = features_ps
         self.num_item = num_item
         self.train_mat = train_mat
         self.num_ng = num_ng
@@ -48,7 +52,10 @@ class TrainSet(data.Dataset):
                     j = np.random.randint(self.num_item)
                 self.features_ng.append([u, j])
 
-        labels_ps = [1 for _ in range(len(self.features_ps))]
+        #labels_ps = [1 for _ in range(len(self.features_ps))]
+        labels_ps = []
+        for x in train_data:
+            labels_ps.append(x[2])
         labels_ng = [0 for _ in range(len(self.features_ng))]
         self.features_fill = self.features_ps + self.features_ng
         self.labels_fill = labels_ps + labels_ng
